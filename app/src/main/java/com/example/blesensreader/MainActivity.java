215f5cf6320a199fa69c6289d8e2afc064166d91
shashaaankk <54775855+shashaaankk@users.noreply.github.com>
2024-04-30 17:08:46 +0200

    "groundwork for search and display"

--- app/src/main/java/com/example/blesensreader/MainActivity.java
+++ app/src/main/java/com/example/blesensreader/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.blesensreader;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -25,6 +37,10 @@ public class MainActivity extends AppCompatActivity {
     private boolean isBLUETOOTH_SCAN = false;
     private boolean isBLUETOOTH_ADVERTISE = false;
     private boolean isBLUETOOTH_CONNECT = false;
+    private boolean isACCESS_COARSE_LOCATION = false;
+    private boolean isACCESS_FINE_LOCATION = false;
+    private Button toscanButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,18 +53,33 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        toscanButton = findViewById(R.id.toScanButton);
+        
+        //Transition
+        toscanButton.setOnClickListener(v -> {
+
+            Intent intent = new Intent(MainActivity.this, ScanDispActivity.class);
+            startActivity(intent);
+
+        });
         mPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), new ActivityResultCallback<Map<String, Boolean>>() {
             @Override
             //Update Permissions
             public void onActivityResult(Map<String, Boolean> result) {
-                if(result.get(android.Manifest.permission.BLUETOOTH_SCAN)!=null){
+                if (result.get(android.Manifest.permission.BLUETOOTH_SCAN) != null) {
                     isBLUETOOTH_SCAN = result.get(android.Manifest.permission.BLUETOOTH_SCAN);
                 }
-                if(result.get(android.Manifest.permission.BLUETOOTH_SCAN)!=null){
+                if (result.get(android.Manifest.permission.BLUETOOTH_SCAN) != null) {
                     isBLUETOOTH_ADVERTISE = result.get(android.Manifest.permission.BLUETOOTH_ADVERTISE);
                 }
-                if(result.get(android.Manifest.permission.BLUETOOTH_SCAN)!=null){
+                if (result.get(android.Manifest.permission.BLUETOOTH_SCAN) != null) {
                     isBLUETOOTH_CONNECT = result.get(android.Manifest.permission.BLUETOOTH_CONNECT);
+                }
+                if (result.get(android.Manifest.permission.ACCESS_COARSE_LOCATION) != null) {
+                    isACCESS_COARSE_LOCATION = result.get(android.Manifest.permission.ACCESS_COARSE_LOCATION);
+                }
+                if (result.get(android.Manifest.permission.ACCESS_FINE_LOCATION) != null) {
+                    isACCESS_FINE_LOCATION = result.get(android.Manifest.permission.ACCESS_FINE_LOCATION);
                 }
 
             }
@@ -57,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /*Check if Permission has been Granted and update the boolean flags for permission*/
-    private void requestPermission(){
+    private void requestPermission() {
         isBLUETOOTH_SCAN = ContextCompat.checkSelfPermission(
                 this,
                 android.Manifest.permission.BLUETOOTH_SCAN) == PackageManager.PERMISSION_GRANTED;
@@ -69,9 +100,18 @@ public class MainActivity extends AppCompatActivity {
         isBLUETOOTH_CONNECT = ContextCompat.checkSelfPermission(
                 this,
                 android.Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED;
+
+        isACCESS_COARSE_LOCATION = ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+
+        isACCESS_FINE_LOCATION = ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+
         List<String> permissions = new ArrayList<String>();
 
-        if (!isBLUETOOTH_SCAN){
+        if (!isBLUETOOTH_SCAN) {
             permissions.add(android.Manifest.permission.BLUETOOTH_SCAN);
         }
         if (!isBLUETOOTH_ADVERTISE) {
@@ -80,13 +120,16 @@ public class MainActivity extends AppCompatActivity {
         if (!isBLUETOOTH_CONNECT) {
             permissions.add(android.Manifest.permission.BLUETOOTH_CONNECT);
         }
-
+        if (!isACCESS_COARSE_LOCATION) {
+            permissions.add(android.Manifest.permission.ACCESS_COARSE_LOCATION);
+        }
+        if (!isACCESS_FINE_LOCATION) {
+            permissions.add(android.Manifest.permission.ACCESS_FINE_LOCATION);
+        }
         // Request any permissions that have not been granted
         if (!permissions.isEmpty()) {
             mPermissionLauncher.launch(permissions.toArray(new String[0]));
         }
     }
-
-
 
 }
